44484a4394ccec61c68902692fdac85806372e0b
vyshakgit <vyshak.github@gmail.com>
2026-04-07 00:07:33 +0800

    Update weather

--- src/DemoApi/Controllers/WeatherForecastController.cs
+++ src/DemoApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace my_demo_api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing123", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
